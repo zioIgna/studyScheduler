9339971c5c421e54ef6d758c308ec7449da453c9
Ignazio Carbonaro <ignaziocarbonaro@hotmail.com>
2020-02-08 17:19:57 +0100

    Aggiornata la gestione delle routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { NavigationPage } from './navigation/navigation.page';
 
 const routes: Routes = [
   { path: '', redirectTo: 'auth', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
-  { path: 'argomenti', loadChildren: './argomenti/argomenti.module#ArgomentiPageModule' },
-  { path: 'calendario', loadChildren: './calendario/calendario.module#CalendarioPageModule' },
+  // { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  // { path: 'argomenti', loadChildren: './argomenti/argomenti.module#ArgomentiPageModule' },
+  // { path: 'calendario', loadChildren: './calendario/calendario.module#CalendarioPageModule' },
   { path: 'navigation', loadChildren: './navigation/navigation.module#NavigationPageModule' },
-  { path: 'unit-detail', loadChildren: './unit-detail/unit-detail.module#UnitDetailPageModule' },
+  // { path: 'unit-detail', loadChildren: './unit-detail/unit-detail.module#UnitDetailPageModule' },
   { path: 'auth', loadChildren: './auth/auth.module#AuthPageModule' },
 ];
 
