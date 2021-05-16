279746bc3196602be282438fb2d03af38febcaba
Ignazio Carbonaro <ignaziocarbonaro@hotmail.com>
2021-05-16 21:25:13 +0200

    Navigazione a pagina di autenticazione dopo errore
    Si fa il logout e si naviga a pagina di autenticazione ma dopo la nuova autenticazione bisogna navigare alla pagina di calendario e poi di nuovo alle unità per ottenere che vengano visualizzate le unità

--- src/app/error.service.ts
+++ src/app/error.service.ts
@@ -1,13 +1,13 @@
-import { HttpErrorResponse } from '@angular/common/http';
-import { ErrorHandler, Injectable, Injector } from '@angular/core';
-import { Router } from '@angular/router';
-import { AuthenticationService } from './auth/authentication.service';
+import { HttpErrorResponse } from "@angular/common/http";
+import { ErrorHandler, Injectable, Injector, NgZone } from "@angular/core";
+import { Router } from "@angular/router";
+import { AuthenticationService } from "./auth/authentication.service";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ErrorService implements ErrorHandler {
-  constructor(private injector: Injector) { }
+  constructor(private injector: Injector, private zone: NgZone) {}
 
   handleError(error: Error): void {
     const router = this.injector.get(Router);
@@ -15,10 +15,11 @@ export class ErrorService implements ErrorHandler {
     if (error instanceof HttpErrorResponse) {
       console.log(error.status);
     } else {
-      console.error("an error occurred here broo" + error);
+      console.error("an error occurred here broo: " + error);
     }
     authService.logout();
-    router.navigateByUrl('/auth');
+    this.zone.run(() => {
+      router.navigateByUrl("/auth");
+    });
   }
-
 }
