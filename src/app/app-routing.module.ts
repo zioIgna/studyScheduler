22ee5b85ff3a4528c65dbedb5c2d0f79251c6458
Ignazio Carbonaro <ignaziocarbonaro@hotmail.com>
2021-05-29 18:56:25 +0200

    Eliminazione routes commentate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,20 +4,8 @@ import { NavigationPage } from './navigation/navigation.page';
 
 const routes: Routes = [
   { path: '', redirectTo: 'auth', pathMatch: 'full' },
-  // { path: 'home', loadChildren: './home/home.module#HomePageModule' },
-  // { path: 'argomenti', loadChildren: './argomenti/argomenti.module#ArgomentiPageModule' },
-  // { path: 'calendario', loadChildren: './calendario/calendario.module#CalendarioPageModule' },
   { path: 'navigation', loadChildren: './navigation/navigation.module#NavigationPageModule' },
-  // { path: 'unit-detail', loadChildren: './unit-detail/unit-detail.module#UnitDetailPageModule' },
   { path: 'auth', loadChildren: './auth/auth.module#AuthPageModule' },
-  // { path: 'sources', loadChildren: './sources/sources.module#SourcesPageModule' },
-
-  // { path: 'customization', loadChildren: './customization/customization.module#CustomizationPageModule' },
-
-  // { path: 'reschedule-unit', loadChildren: './reschedule-unit/reschedule-unit.module#RescheduleUnitPageModule' },
-
-  // { path: 'update-unit', loadChildren: './update-unit/update-unit.module#UpdateUnitPageModule' },
-
 ];
 
 @NgModule({
